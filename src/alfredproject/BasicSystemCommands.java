eae6556d8b1c36ca0c96f0b43a132c74ca26ed75
Benjamin Zhang <beneric633@gmail.com>
2013-04-28 15:30:57 -0500

    not working lock command

--- src/alfredproject/BasicSystemCommands.java
+++ src/alfredproject/BasicSystemCommands.java
@@ -3,6 +3,10 @@
  * and open the template in the editor.
  */
 package alfredproject;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  *
@@ -44,12 +48,37 @@ public static Alfredproject ap = new Alfredproject();
     }
     private static void LockComputer(){
         //locks computer
+        //try statement for robot input windows - L
+        //doesn't work so far.
+        try{
+        Robot robot = new Robot();
+        robot.keyPress(KeyEvent.VK_WINDOWS);
+        robot.keyPress(KeyEvent.VK_L);
+        
+        //sets timer
+            Timer timer= new Timer();
+            timer.schedule(new TimerTask(){
+                @Override
+                public void run(){
+                    //blank for timer
+                }
+            },1000);
+        //end of timer results in release of keys
+            
+        robot.keyRelease(KeyEvent.VK_WINDOWS);
+        robot.keyRelease(KeyEvent.VK_L);
+        
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        //output statement
         ap.OutputString = ap.FinishingStatement;
-        //to finish later likely to simulate keypresses of windows-L
+        
     }
     private static void SleepComputer(){
         //puts computer to sleep
         ap.OutputString = ap.FinishingStatement;
         //to finish later likely to simulate keypresses of fn-f1
+        
     }
 }
